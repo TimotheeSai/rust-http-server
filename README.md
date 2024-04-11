bbf0e1ff2ec8f158bd92a104cac350c2ef1804d9
Timothee Saisset <timothee.saisset@spacefill.fr>
2024-04-11 19:13:01 +0200

    feat: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Rust http server
+
+Basic http server for debug purpose.
+
+Coded from [Final Project: Building a Multithreaded Web Server - The Rust Programming Language](https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html)
+
+## Notes
+
+**Local doc:**
+
+```sh
+rustup doc
+```
+
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use rust_http_server::ThreadPool;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let uri = http_request[0].split_whitespace().collect::<Vec<&str>>()[1];
+    let (status_line, filename) = match &uri[..] {
+        "/" => ("HTTP/1.1 200 OK", "index.html"),
+        "/sleep" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(format!("./src/content/{filename}")).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let pool = ThreadPool::new(10);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+    println!("Shutting down.");
+}
